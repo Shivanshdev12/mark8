694138c01af023003fc101529264d54ae6e268e2
Shivansh Mehrotra <shivansh.sm1@gmail.com>
2021-10-25 03:00:42 +0530

    First commit�

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+//small DataBase
+const emojiDictionary = {
+  "😊": "Smilling",
+  "😪": "Sleepy",
+  "😢": "Crying face",
+  "🤡": "Clown",
+  "🤓": "Nerd face",
+  "🐱‍👤": "Ninja cat",
+};
 
 function App() {
+  const [meaning, setMeaning] = useState("");
+  var emojiinDB = Object.keys(emojiDictionary);
+  function emojiInputHandler(e) {
+    var userInput = e.target.value;
+    var meaning = emojiDictionary[userInput];
+    if (meaning === undefined) {
+      setMeaning("we don't have this yet in our DataBase.");
+    } else {
+      setMeaning(meaning);
+    }
+  }
+
+  function clickHandler(emoji) {
+    setMeaning(emojiDictionary[emoji]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <h1>Emoji Interpreter</h1>
+      <input onChange={emojiInputHandler}></input>
+      <h3>{meaning}</h3>
+      {emojiinDB.map(function (emoji) {
+        return (
+          <span
+            onClick={() => clickHandler(emoji)}
+            style={{ fontSize: "1.5rem", padding: "0.5rem" }}
+          >
+            {emoji}
+          </span>
+        );
+      })}
     </div>
   );
 }
